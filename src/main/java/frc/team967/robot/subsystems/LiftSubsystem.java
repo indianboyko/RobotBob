f0231ff4cc250723f21996968e4a30c2d4202078
Matt Behnke <iterevo@gmail.com>
2018-03-22 19:01:43 -0500

    Last update on practice day at comp

--- src/main/java/frc/team967/robot/subsystems/LiftSubsystem.java
+++ src/main/java/frc/team967/robot/subsystems/LiftSubsystem.java
@@ -1,16 +1,12 @@
 package frc.team967.robot.subsystems;
 
 import frc.team967.robot.Robot;
-import frc.team967.robot.RobotConstraints;
 import frc.team967.robot.RobotMap;
 import frc.team967.robot.commands.LiftMove;
-import frc.team967.robot.commands.T_ArcadeDriveLookUp;
-
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -27,18 +23,13 @@ public class LiftSubsystem extends Subsystem {
 	
 	private DoubleSolenoid liftShift;
 	private DoubleSolenoid busterBrake;
-	
-//	private final double kP = RobotConstraints.LiftSubsystem_kP;
-//	private final double kI = RobotConstraints.LiftSubsystem_kI;
-//	private final double kD = RobotConstraints.LiftSubsystem_kD;
-//	private final double kToleranceDegrees = RobotConstraints.LiftSubsystem_kTolerance;
-	
+
     public LiftSubsystem(){
     	liftLead = new WPI_TalonSRX(RobotMap.liftLead);
     	liftFollow = new WPI_TalonSRX(RobotMap.liftFollow);
     	
     	liftLead.configSelectedFeedbackSensor(com.ctre.phoenix.motorcontrol.FeedbackDevice.QuadEncoder, 0, 0);
-    	liftLead.setSensorPhase(false);
+    	liftLead.setSensorPhase(true);
     	
     	liftFollow.follow(liftLead);
     	liftFollow.setInverted(true);
@@ -52,16 +43,16 @@ public class LiftSubsystem extends Subsystem {
         
     public boolean IsTop()    { return limitSwitchTop.get();    }
     public boolean IsBottom() { return limitSwitchBottom.get(); }
-    
+
+    Boolean pos;
+
     public double Position() {
     	if (limitSwitchBottom.get()) {
     		liftLead.getSensorCollection().setQuadraturePosition(0, 5);
-            SmartDashboard.putBoolean("PositionInit", true);
     		return liftLead.getSensorCollection().getQuadraturePosition();
     	}
     	else {
-            SmartDashboard.putBoolean("PositionInit", false);
-    		return liftLead.getSensorCollection().getQuadraturePosition();
+           return liftLead.getSensorCollection().getQuadraturePosition();
     	}
     }
 
@@ -102,10 +93,6 @@ public class LiftSubsystem extends Subsystem {
     	SmartDashboard.putNumber("Lift Velocity", liftLead.getSelectedSensorVelocity(0));
     	SmartDashboard.putNumber("LiftAmpsLead", liftLead.getOutputCurrent());
     	SmartDashboard.putNumber("LiftAmpsFollow", liftFollow.getOutputCurrent());
-    	
-    	SmartDashboard.putBoolean("limmit botom", IsBottom());
-
-    	SmartDashboard.putNumber("LIFT Encoder", liftLead.getSensorCollection().getQuadraturePosition());
     }
 }
 
